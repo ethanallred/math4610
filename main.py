6b039944015157fc32087527c48abb77800d2471
ethanallred <ethan45allred@gmail.com>
2022-09-16 23:50:29 -0600

    Added Comments

--- main.py
+++ main.py
@@ -3,12 +3,46 @@ import math
 import tabulate
 from tabulate import tabulate
 def function1 (x):
+    '''
+    Function: Function 1
+
+    Summary: Solves Xe(-x) with the input of x
+    :param x:
+    :type x: float
+    :return f:
+    :rtype: float
+
+    '''
     f = x * math.pow(np.e , -x)
     return f
 def function2 (x):
-    f = x * math.pow(np.e , -x)
+    '''
+    Function: Function 2
+
+    Summary: Solves 10.14 * e(^(x^(2)))cos(pi/x)
+
+    :param x:
+    :type x: float
+    :return f:
+    :rtype: float
+
+    '''
+    f = 10.14*math.pow(np.e, x^2)* np.cos(np.pi/x)
     return f
 def fixed1original(x,f,root):
+    '''
+        Function: fixed 1 original
+
+        Summary:  Runs first fixed point equation, and prints a table
+                of the results. Solves 10.14 * e(^(x^(2)))cos(pi/x)
+        :param x:
+        :type x: float
+        :param: f
+        :type f: float
+        :param: root
+        :type root: float
+
+        '''
     maxit = 50
     tol = 0.02
     err = 1
@@ -25,6 +59,19 @@ def fixed1original(x,f,root):
     print(tabulate(table))
 
 def fixed2original(x,f,root):
+    '''
+        Function: fixed 2 original
+
+        Summary: Runs second fixed point equation, and prints a table
+                of the results. Solves 10.14 * e(^(x^(2)))cos(pi/x)
+        :param x:
+        :type x: float
+        :param: f
+        :type f: float
+        :param: root
+        :type root: float
+
+    '''
     maxit = 50
     tol = 0.02
     err = 1
@@ -41,6 +88,20 @@ def fixed2original(x,f,root):
     print(tabulate(table))
 
 def fixed1fix(x,f,root):
+    '''
+        Function: fixed 1 fix
+
+        Summary: Runs a modified version of the first fixed point equation,
+            and prints a table of the results. Solves 10.14 * e(^(x^(2)))cos(pi/x)
+
+        :param x:
+        :type x: float
+        :param: f
+        :type f: float
+        :param: root
+        :type root: float
+
+    '''
     maxit = 50
     tol = 0.02
     err = 1
@@ -55,7 +116,22 @@ def fixed1fix(x,f,root):
         table.append([str(i), str(x), str(err)])
     print("Table for adjusted fixed method 1")
     print(tabulate(table))
+
 def fixed2fix(x,f,root):
+    '''
+        Function: fixed 2 fix
+
+        Summary: Runs a modified version of the second fixed point equation,
+            and prints a table of the results. Solves 10.14 * e(^(x^(2)))cos(pi/x)
+
+        :param x:
+        :type x: float
+        :param: f
+        :type f: float
+        :param: root
+        :type root: float
+
+        '''
     maxit = 50
     tol = .02
     err = 1
@@ -72,10 +148,37 @@ def fixed2fix(x,f,root):
     print(tabulate(table))
 
 def bihowmanyit(tol,a,b):
+    '''
+        Function: bi how many it
+
+        Summary: Finds how many iterations the bi section method will have to take
+
+        :param: tol
+        :type tol: float
+        :param: a
+        :type a: float
+        :param: b
+        :type b: float
+        :return k:
+        :rtype: float
+
+    '''
     k = (np.log(tol)/np.log(b-a))/(-np.log(2))+1
     return k
 
 def bisection(a,b,k):
+    '''
+        Function: bisection
+
+        Summary: Runs the bi section method
+
+        :param: a
+        :type a: float
+        :param: b
+        :type b: float
+        :param: k
+        :type k:
+    '''
     i = 0
     while i < k:
         x = (a+b)/2
