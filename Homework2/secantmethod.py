a333e6a4f79ff3e525deb588e4e9296c8df84e35
ethanallred <113744596+ethanallred@users.noreply.github.com>
2022-09-26 09:43:49 -0600

    Update secantmethod.py

--- Homework2/secantmethod.py
+++ Homework2/secantmethod.py
@@ -1,5 +1,5 @@
-def secantmethod(x0, x1, func0, func1)
+def secantmethod(x0, x1)
     xhold = x1
-    x1 = (func1 *  (x0 -x1)) / (func0 - func1)
+    x1 = (function(x1) *  (x0 -x1)) / (function(0) - function(1))
     x0 = xhold
     return x1 and x0
