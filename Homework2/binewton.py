eea57da5eec67f837d4b23b0460c3108c50b528b
ethanallred <113744596+ethanallred@users.noreply.github.com>
2022-09-26 15:04:48 -0600

    Create binewton.py

---
+++ Homework2/binewton.py
@@ -0,0 +1,14 @@
+def binewton(x0, x1):
+    newtonmethod(x0)
+    
+    i = 0
+    while i < 4:
+        xr = (x0+x1)/2
+        fx = function (xr)
+        fx1 = function (x1)
+        if (fx * fb) < 0:
+            x0 = x
+        else:
+            x1 = x
+        i = i + 1
+  newtonmethod(
